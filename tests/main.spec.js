8e88035efb4a953bea5fa4dbc1626472d3231f81
Daniele Minatto <12018609+dminatto@users.noreply.github.com>
2021-07-25 18:36:56 -0300

    feat: add smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,32 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+import {
+  search,
+  searchAlbuns,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+} from "../src/main.js";
 
-describe('Main', () => {
+describe("Spotify Wrapper", () => {
+  describe("Smoke tests", () => {
+    it("should exists the search method", () => {
+      expect(search).to.exist;
+    });
 
+    it("should exists the searchAlbuns method", () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it("should exists the searchArtists method", () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it("should exists the searchTracks method", () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it("should exists the searchPlaylists method", () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
 });
